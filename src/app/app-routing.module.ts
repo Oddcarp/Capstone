70746e7c2e709350a9a4cac2f080b0821bdefa23
Ray <mkeller1991@gmail.com>
2018-07-17 17:21:58 -0500

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { SpdfComponent } from './spdf/spdf.component';
+import { AmsdComponent } from './amsd/amsd.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: WelcomePageComponent },
+  { path: 'spdf',      component: SpdfComponent },
+  { path: 'AMSD',      component: AmsdComponent },
+  { path: '', redirectTo: '/Home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
